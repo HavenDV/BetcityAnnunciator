ac8c142dcacd3c464a7d3f453c376b3cef0de6c9
HavenDV <havendv@gmail.com>
2018-07-02 21:11:57 +1000

    Added minimal Cef

--- BetcityAnnunciator/App.xaml.cs
+++ BetcityAnnunciator/App.xaml.cs
@@ -1,6 +1,21 @@
-﻿namespace BetcityAnnunciator
+﻿using System;
+using System.IO;
+using CefSharp;
+
+namespace BetcityAnnunciator
 {
     public partial class App
     {
+        public App()
+        {
+            var settings = new CefSettings()
+            {
+                //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+            };
+
+            //Perform dependency check to make sure all relevant resources are in our output directory.
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+        }
     }
 }
---
+++ BetcityAnnunciator/Converters/TitleConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BetcityAnnunciator.Converters
+{
+    public class TitleConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "BetcityAnnunciator - " + (value ?? "No Title Specified");
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
